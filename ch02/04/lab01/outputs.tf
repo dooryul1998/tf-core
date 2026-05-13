83a525bcd90f936b63925e5254c42a66aeaae1cf
dooryul1998 <dooryul1998@gmail.com>
2026-05-13 09:33:17 +0900

    ch02 폴더 추가

---
+++ ch02/04/lab01/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_web_id" {
+  value = aws_instance.this.id
+}
+
+output "instance_web_public_ip" {
+  value = aws_instance.this.public_ip
+}
+
+output "sg_instance_web_id" {
+  value = aws_security_group.this.id
+}
+
+output "sg_instance_web_name" {
+  value = aws_security_group.this.name
+}
+
+output "sg_instance_web_iam_role" {
+  value = aws_iam_role.this.name
+}
